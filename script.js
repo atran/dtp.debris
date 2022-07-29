bd9d08944d6c4986676907ce4488c4c34f5c0757
anthony tran <atran@users.noreply.github.com>
2022-07-29 13:09:10 -0700

    reformat animations

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
-var $element1 = $(".p1");
+// Hover + scale up paragraphs
+$("p").on("mouseenter", function() {
+  $el = $(this);
+  console.log($el);
+  // TODO: Fill in velocity animation below to scale up
+  $el.velocity();
+})
 
-var mySequence = [
-  { e: $element1, p: {position: "relative"}, o: {visibility: "hidden", delay: 4000}},
-  { e: $element1, p: {translateX: -500, translateY: 800}, o: {duration: 1000}},
-  { e: $element1, p: {position: "relative"}, o: {visibility: "visible", delay: 1000}},
-  { e: $element1, p: {position: "relative"}, o: {visibility: "hidden", delay: 5000}},
-  { e: $element1, p: {translateX: 0, translateY: 0}, o: {duration: 1000}},
-  { e: $element1, p: {position: "relative"}, o: {visibility: "visible", delay: 1000}}
-];
-
- $.Velocity.RunSequence(mySequence);
+$("p").on("mouseout", function() {
+  $el = $(this);
+  console.log($el);
+  // TODO: Fill in velocity animation below to scale it back down
+  $el.velocity();
+})
 
 
-var $element2 = $(".p2");
 
+// Ambient paragraph movement
+// Paragraph 1
+var $element1 = $("#p1");
+var mySequence1 = [
+    { e: $element1, p: {position: "relative"}, o: {visibility: "visible", delay: 400}},
+    { e: $element1, p: {translateX: -500, translateY: 800}, o: {duration: 100}},
+    { e: $element1, p: {position: "relative"}, o: {visibility: "visible", delay: 100}},
+    { e: $element1, p: {position: "relative"}, o: {visibility: "visible", delay: 500}},
+    { e: $element1, p: {translateX: 0, translateY: 0}, o: {duration: 100}},
+    { e: $element1, p: {position: "relative"}, o: {visibility: "visible", delay: 100}}
+  ];
+$.Velocity.RunSequence(mySequence1);
+var interval = setInterval(() => {
+  $.Velocity.RunSequence(mySequence1);
+}, 1300);
+
+// Paragraph 2
+var $element2 = $("#p2");
 var mySequence2 = [
   { e: $element2, p: {position: "relative"}, o: {visibility: "hidden", delay: 6000}},
   { e: $element2, p: {translateX: -200, translateY: -200}, o: {duration: 1000}},
@@ -22,14 +41,10 @@ var mySequence2 = [
   { e: $element2, p: {translateX: 0, translateY: 0}, o: {duration: 1000}},
   { e: $element2, p: {position: "relative"}, o: {visibility: "visible", delay: 1000}}
 ];
-
- $.Velocity.RunSequence(mySequence2);
-
-
-
-// var interval = setInterval(() => {
-//   $.Velocity.RunSequence(mySequence);
-// }, 6000);
+$.Velocity.RunSequence(mySequence2);
+var interval = setInterval(() => {
+  $.Velocity.RunSequence(mySequence2);
+}, 15000);
 
 
 // $body.velocity(
